00e078ad8e0825890c8e6eb421ca5fc80659d296
amogus <74731114+amogusimposter123123@users.noreply.github.com>
2021-12-30 22:52:25 +0200

    Create keysys.lua

---
+++ keysys.lua
@@ -0,0 +1,19 @@
+
+local plr = game.Players.LocalPlayer--чекает локал плеера(тебя)
+if getgenv().Key=="X%LZbvK8;w}Qse.q?uBD" then--чекает если правильный ключ
+print("[!] Right Key!")
+local synx = syn--синапс
+local krnl = KRNL_LOADED--крнл
+local electron = IsElectron--електрон
+if synx then--чекает если заинжектин синапс
+print("[!] = Synapse X is Whitelisted for this Script! Enjoy playing!")
+else
+if krnl then--чекает если заинжектин крнл
+plr:Kick("[!] = Krnl is not Whitelisted for this Script!")
+else
+if electron then--чекает если заинжектин електрон
+plr:Kick("[!] = Electron is not Whitelisted for this Script!")
+end
+end
+end
+end
