e2b19e653778bc5c5f4566114607039140b30488
amogus <74731114+amogusimposter123123@users.noreply.github.com>
2021-12-31 15:52:53 +0200

    Update keysys.lua

--- keysys.lua
+++ keysys.lua
@@ -1,5 +1,4 @@
 
-local plr = game.Players.LocalPlayer--чекает локал плеера(тебя)
 if getgenv().Key=="X%LZbvK8;w}Qse.q?uBD" then--чекает если правильный ключ
 print("[!] Right Key!")
 local synx = syn--синапс
@@ -9,10 +8,10 @@ if synx then--чекает если заинжектин синапс
 print("[!] = Synapse X is Whitelisted for this Script! Enjoy playing!")
 else
 if krnl then--чекает если заинжектин крнл
-plr:Kick("[!] = Krnl is not Whitelisted for this Script!")
+game.Players.LocalPlayer:Kick("[!] = Krnl is not Whitelisted for this Script!")
 else
 if electron then--чекает если заинжектин електрон
-plr:Kick("[!] = Electron is not Whitelisted for this Script!")
+game.Players.LocalPlayer:Kick("[!] = Electron is not Whitelisted for this Script!")
 end
 end
 end
